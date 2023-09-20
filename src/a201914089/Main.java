6e2f064f5933f63d1c912083ce8d30a504bae141
djdongjae <jk47wee@gmail.com>
2023-09-20 10:28:03 +0900

    five

--- src/a201914089/Main.java
+++ src/a201914089/Main.java
@@ -1,9 +1,12 @@
 package a201914089;
 
 public class Main {
+
   public static void main(String[] args) {
+    Five.print();
     Two.print();
     System.out.println("one 201914089");
     Three.print();
   }
 }
+
